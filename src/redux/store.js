0938ce2e3e9100d21d916bc5fc0f35b8d9d1c40e
DD_THANG <thangboiahi@gmail.com>
2020-06-19 01:08:29 +0530

    added react-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import {createStore, applyMiddleware} from "redux";
+import {persistStore} from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
@@ -9,4 +10,7 @@ const store = createStore(
   rootReducer, applyMiddleware(...middlewares)
 );
 
-export default store;
+// create persisted version of store
+const persistor = persistStore(store);
+
+export {store, persistor};
